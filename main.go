fc082ee78955bdc99288b945573ee29524768699
Vít Kozel <vitek.kozel@email.cz>
2023-03-04 17:42:40 +0100

    done

--- main.go
+++ main.go
@@ -15,24 +15,14 @@ func main() {
 	}
 	fmt.Println(args)
 
-	// convert args to string
-	args = strings.Split(args, " ")[0]
-
-	if len(args) == 0 {
-		fmt.Println("Please enter a Google Drive link!")
-		os.Exit(1)
-	}
-
 	final := ""
-	enter := args[0]
+	enter := args
 	enter = strings.Split(enter, "/d/")[1]
-
 	if strings.HasSuffix(enter, "/view") {
 		enter = strings.Split(enter, "/view")[0]
 	} else {
 		enter = strings.Split(enter, "/edit")[0]
 	}
-
 	final = "https://drive.google.com/uc?id=" + enter
 	fmt.Println(final)
 }
